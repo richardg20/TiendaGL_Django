6836d5ce660b372705f87b446b24dbe2042eccea
richardg20 <rich.gutierrez@duocuc.cl>
2023-06-20 23:35:29 -0400

    xd

--- tiendaweb/models.py
+++ tiendaweb/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    precio = models.IntegerField(max_length=7)
+    descripcion = models.CharField(max_length=500, default="Descripcion")
+    imagen = models.ImageField(upload_to='imagenes/')
+
+    def __str__(self):
+        return self.nombre
