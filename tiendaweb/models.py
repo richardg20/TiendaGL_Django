c626b82ff172ca0cc96cb4a17b0179f23f78f9f7
richardg20 <rich.gutierrez@duocuc.cl>
2023-06-21 00:20:09 -0400

    aaaaaaaaa

--- tiendaweb/models.py
+++ tiendaweb/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
-    precio = models.IntegerField(max_length=7)
+    precio = models.IntegerField()
     descripcion = models.CharField(max_length=500, default="Descripcion")
+    anno_lanzamiento = models.CharField(max_length=4, default="0")
+    clasificacion  = models.CharField(max_length=2,default="")
+    desarrollador = models.CharField(max_length=50,default="")
+    genero = models.CharField(max_length=100,default="")
+    id_video = models.CharField(max_length=30, default="")
     imagen = models.ImageField(upload_to='imagenes/')
 
     def __str__(self):
